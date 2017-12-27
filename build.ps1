5f111e00aa0e38ec6c42ed898161a05373ebf88c
Chandramohan <jchandramohan@hotmail.com>
2017-12-27 12:03:29 -0500

    Download script fix

--- build.ps1
+++ build.ps1
@@ -4,15 +4,16 @@ $nugetPath = "$env:LOCALAPPDATA\NuGet\NuGet.exe"
 if (!(Get-Command NuGet -ErrorAction SilentlyContinue) -and !(Test-Path $nugetPath))
 {
 	Write-Host "Downloading NuGet.exe..."
-	<#
 	$downloadUrl = "https://dist.nuget.org/win-x86-commandline/latest/nuget.exe"
-	(New-Object System.Net.WebClient).DownloadFile($downloadUrl, $nugetPath)
-	#>
-
+	#(New-Object System.Net.WebClient).DownloadFile($downloadUrl, $nugetPath)
 	$web = New-Object System.Net.WebClient
-	$credentials = [System.Text.Encoding]::ASCII.GetBytes("admin:P@ssw0rd123")
+
+	<#
+	$web = New-Object System.Net.WebClient
+	$credentials = [System.Text.Encoding]::ASCII.GetBytes("user:pwd")
 	$credentialsB64S = [System.Convert]::ToBase64String($credentials)
 	$web.Headers[[System.Net.HttpRequestHeader]::Authorization] = "Basic" + $credentialsB64S
+	#>
 
 	$web.DownloadFile($downloadUrl, $nugetPath)
 }
