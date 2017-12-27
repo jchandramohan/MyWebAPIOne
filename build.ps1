f0e4e3c59d34007d7914c8aa8ce4b484a9127ec2
Chandramohan <jchandramohan@hotmail.com>
2017-12-26 21:32:14 -0500

    File download with credentials

--- build.ps1
+++ build.ps1
@@ -4,8 +4,17 @@ $nugetPath = "$env:LOCALAPPDATA\NuGet\NuGet.exe"
 if (!(Get-Command NuGet -ErrorAction SilentlyContinue) -and !(Test-Path $nugetPath))
 {
 	Write-Host "Downloading NuGet.exe..."
+	<#
 	$downloadUrl = "https://dist.nuget.org/win-x86-commandline/latest/nuget.exe"
 	(New-Object System.Net.WebClient).DownloadFile($downloadUrl, $nugetPath)
+	#>
+
+	$web = New-Object System.Net.WebClient
+	$credentials = [System.Text.Encoding]::ASCII.GetBytes("admin:P@ssw0rd123")
+	$credentialsB64S = [System.Convert]::ToBase64String($credentials)
+	$web.Headers[[Sytstem.Net.HttpRequestHeader]::Authorization] = "Basic" + $credentialsB64S
+
+	$web.DownloadFile($downloadUrl, $nugetPath)
 }
 else
 {
